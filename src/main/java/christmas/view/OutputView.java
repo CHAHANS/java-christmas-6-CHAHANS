c3f7b6e9a10b5e56c816e66266e75830841fab88
CHAHANS <herodev0129@gmail.com>
2023-11-15 13:28:41 +0900

    refactor(view): OutPutView에서 사용되는 enum 수정, 메세지 확인 테스트코드 작성

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,92 +2,87 @@ package christmas.view;
 
 import christmas.domain.Bills;
 import christmas.domain.Customer;
-import christmas.domain.EventMenu;
 import christmas.domain.Order;
 import christmas.domain.decemberEvent.DecemberEvent;
+
 import static christmas.messages.OutputFormatMessage.*;
 
-import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.util.Map;
 
 public class OutputView {
-    private final DecimalFormat priceFormat;
-
-    public OutputView() {
-        this.priceFormat = new DecimalFormat(PRICE_FORMAT.getMessage());
-    }
 
     public void printSummeryStarter(Customer customer) {
         LocalDate date = customer.getOrderDate();
-        System.out.println(date.getMonthValue() + "월 " + date.getDayOfMonth() +
-                "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!" + LINE_SEPARATOR);
+        System.out.printf(SUMMERY_START_MESSAGE.getMessage(), date.getMonthValue(), date.getDayOfMonth());
+        System.out.println();
     }
 
     public void printOrderAll(Customer customer) {
-        System.out.println("<주문 메뉴>");
+        System.out.printf(ORDER_ALL_TITLE.getMessage());
         for (Order order : customer.getMyOrder()) {
-            System.out.println(order.getOrderMenu().getName() + SPACES + order.getEA() + EA + LINE_SEPARATOR);
+            System.out.printf(PRINT_PRODUCT_EA_FORMAT.getMessage(), order.getOrderMenu().getName(), order.getEA());
         }
+        System.out.println();
     }
 
     public void printPayBeforeBenefit(Customer customer) {
-        System.out.println("<할인 전 총주문 금액>");
-        System.out.println(priceFormatChanger(customer.getTotalProductPrice()) + WON);
+        System.out.printf(PAY_BEFORE_BENEFIT_TITLE.getMessage());
+        System.out.printf(PRINT_PRICE_KRW_FORMAT.getMessage(), customer.getTotalProductPrice());
         System.out.println();
     }
 
     public void printGiveEventProduct(Bills bills) {
-        System.out.println("<증정 메뉴>");
-        Integer values = bills.getApprovedEvent().get(DecemberEvent.GIFT_EVENT.getKoreanName());
-        if (values == null) {
-            System.out.println(NOTTING);
+        System.out.printf(GIFT_EVENT_PRODUCT_TITLE.getMessage());
+        String giftProduct = DecemberEvent.GIFT_EVENT.getKoreanName();
+        if (bills.getApprovedEvent().get(giftProduct) == null) {
+            System.out.printf(NOTTING.getMessage());
         } else {
-            System.out.println(EventMenu.샴페인.getName() + SPACES + 1 + EA);
+            System.out.printf(PRINT_PRODUCT_EA_FORMAT.getMessage(), giftProduct, 1);
         }
         System.out.println();
     }
 
     public void printBenefitALL(Bills bills) {
-        System.out.println("<혜택 내역>");
-        if (bills.getTotalBenefit() == 0) {
-            System.out.println(NOTTING);
+        System.out.printf(BENEFIT_ALL_TITLE.getMessage());
+        if (bills.getTotalBenefit() <= 0) {
+            System.out.printf(NOTTING.getMessage());
         } else {
             for (Map.Entry<String, Integer> event : bills.getApprovedEvent().entrySet()) {
-                String value = priceFormatChanger(Math.negateExact(event.getValue()));
-                System.out.println(event.getKey() + ": " + value + WON);
+                System.out.printf(PRINT_EVENTS_BENEFIT_FORMAT.getMessage()
+                        , event.getKey()
+                        , Math.negateExact(event.getValue())
+                );
             }
         }
         System.out.println();
     }
 
     public void printTotalBenefitSum(Bills bills) {
-        System.out.println("<총혜택 금액>");
-        int value = bills.getTotalBenefit();
-        if (value == 0) {
-            System.out.println(NOTTING);
+        System.out.printf(BENEFIT_SUM_TITLE.getMessage());
+        int benefitSum = bills.getTotalBenefit();
+        if (benefitSum <= 0) {
+            System.out.printf(NOTTING.getMessage());
         } else {
-            System.out.println(priceFormatChanger(Math.negateExact(value)) + WON);
+            System.out.printf(PRINT_PRICE_KRW_FORMAT.getMessage(), Math.negateExact(benefitSum));
         }
         System.out.println();
     }
 
     public void printPayAfterBenefit(Bills bills, Customer customer) {
-        System.out.println("<할인 후 예상 결제 금액>");
-        int value = customer.getTotalProductPrice() - bills.getTotalBenefit();
-        System.out.println(priceFormatChanger(value) + WON + LINE_SEPARATOR);
+        System.out.printf(PAY_AFTER_BENEFIT_TITLE.getMessage());
+        int payValue = customer.getTotalProductPrice() - bills.getTotalBenefit();
+        System.out.printf(PRINT_PRICE_KRW_FORMAT.getMessage(), payValue);
+        System.out.println();
     }
 
     public void printDecemberBadge(Bills bills) {
-        System.out.println("<12월 이벤트 배지>");
+        System.out.printf(DECEMBER_BADGE_TITLE.getMessage());
         if (bills.getBadges() == null) {
-            System.out.println(NOTTING);
+            System.out.printf(NOTTING.getMessage());
         } else {
             System.out.println(bills.getBadges().getKoreanBadgeName());
         }
-    }
-
-    private String priceFormatChanger(int value) {
-        return priceFormat.format(value);
+        System.out.println();
     }
 }
